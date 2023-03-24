592e208f79d2b738d997012b0570976492c08b55
ddophi98 <lightcloud98@gmail.com>
2023-03-24 20:22:57 +0900

    웹크롤링 코드 완성

--- WebCrawling.py
+++ WebCrawling.py
@@ -1,53 +1,112 @@
 import requests
 from bs4 import BeautifulSoup
-import selenium
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import re
-from time import sleep
 import pandas as pd
 
+
+# 특정 위치의 질병 정보 출력하기
+def print_disease(loc):
+    print('--symptom_sum--\n' + disease_data['symptom_sum'][loc] + '\n')
+    print('--related_disease--\n' + disease_data['related_disease'][loc] + '\n')
+    print('--medical_department--\n' + disease_data['medical_department'][loc] + '\n')
+    print('--synonym--\n' + disease_data['synonym'][loc] + '\n')
+    print('--definition--\n' + disease_data['definition'][loc] + '\n')
+    print('--cause--\n' + disease_data['cause'][loc] + '\n')
+    print('--symptom--\n' + disease_data['symptom'][loc] + '\n')
+    print('--diagnosis--\n' + disease_data['diagnosis'][loc] + '\n')
+    print('--cure--\n' + disease_data['cure'][loc] + '\n')
+
+# 웹크롤링하기
+def web_crawling(disease_data):
+    # 서울아산병원 질환백과
+    url = "https://www.amc.seoul.kr/asan/healthinfo/disease/diseaseList.do?searchKeyword="
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    # 전체 페이지가 얼마나 되는지 확인
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'html.parser')
+    last_page = soup.find('a', class_='lastPageBtn')
+    last_page = int(re.findall('[0-9]+', str(last_page))[0])
+
+    # 웹 크롤링 시작
+    cnt = 0
+    for page in range(last_page):
+
+        # 해당 페이지 안에 있는 질병 목록 확인
+        diseases = driver.find_elements(By.CLASS_NAME, 'contTitle')
+
+        # 각 질병 설명을 보기 위해 순회하면서 클릭
+        for disease in diseases:
+            print(cnt)
+            cnt += 1
+            disease.find_element(By.TAG_NAME, 'a').click()
+
+            # 정보 추출 (구현 예정)
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+
+            # 1. symptom_sum, related_disease_sum, medical_department_sum
+            cont_box_title = soup.find('div', class_='contBox').find('dl').find_all('dt')
+            cont_box_content = soup.find('div', class_='contBox').find('dl').find_all('dd')
+
+            symptom_sum, related_disease, medical_department, synonym = '', '', '', ''
+            for idx, title in enumerate(cont_box_title):
+                if title.text == '증상':
+                    symptom_sum = ', '.join([x.text for x in cont_box_content[idx].find_all('a')])
+                elif title.text == '관련질환':
+                    related_disease = ', '.join([x.text for x in cont_box_content[idx].find_all('a')])
+                elif title.text == '진료과':
+                    medical_department = ', '.join([x.text for x in cont_box_content[idx].find_all('a')])
+                elif title.text == '동의어':
+                    synonym = re.sub('\n|\xa0', '', cont_box_content[idx].text).strip()
+
+            # 2. definition, cause, symptom, diagnosis, cure
+            cont_description_title = soup.find('div', class_='contDescription').find('dl').find_all('dt')
+            cont_description_content = soup.find('div', class_='contDescription').find('dl').find_all('dd')
+
+            definition, cause, symptom, diagnosis, cure = '', '', '', '', ''
+            for idx, title in enumerate(cont_description_title):
+                if title.text == '정의':
+                    definition = re.sub('\n|\xa0', '', cont_description_content[idx].text).strip()
+                elif title.text == '원인':
+                    cause = re.sub('\n|\xa0', '', cont_description_content[idx].text).strip()
+                elif title.text == '증상':
+                    symptom = re.sub('\n|\xa0', '', cont_description_content[idx].text).strip()
+                elif title.text == '진단':
+                    diagnosis = re.sub('\n|\xa0', '', cont_description_content[idx].text).strip()
+                elif title.text == '치료':
+                    cure = re.sub('\n|\xa0', '', cont_description_content[idx].text).strip()
+
+            # 정보 추가
+            disease_data.loc[len(disease_data)] = [symptom_sum, related_disease, medical_department, synonym,
+                                                   definition, cause, symptom, diagnosis, cure]
+
+            # 뒤로 가기
+            driver.back()
+
+        # 페이지 이동
+        if page != last_page - 1:
+            driver.find_element(By.CLASS_NAME, 'nextPageBtn').click()
+
+
 # 데이터 형식 정의
-disease_data = pd.DataFrame({
-    'symptom_sum': [],
-    'related_disease_sum': [],
-    'medical_department_sum': [],
-    'definition': [],
-    'cause': [],
-    'symptom': [],
-    'diagnosis': [],
-    'cure': [],
-})
+disease_data = pd.DataFrame(
+    columns=[
+        'symptom_sum',          # 증상(요약)
+        'related_disease',      # 관련 질환
+        'medical_department',   # 진료과
+        'synonym',              # 동의어
+        'definition',           # 정의
+        'cause',                # 원인
+        'symptom',              # 증상
+        'diagnosis',            # 진단
+        'cure'                  # 치료
+    ]
+)
 
-# 서울아산병원 질환백과
-url = "https://www.amc.seoul.kr/asan/healthinfo/disease/diseaseList.do?searchKeyword="
-driver = webdriver.Chrome()
-driver.get(url)
+web_crawling(disease_data)
+disease_data.to_xml('disease_data.xml')
 
-# 전체 페이지가 얼마나 되는지 확인
-html = requests.get(url).text
-soup = BeautifulSoup(html, 'html.parser')
-last_page = soup.find('a', class_='lastPageBtn')
-last_page = int(re.findall('[0-9]+', str(last_page))[0])
-
-# 웹 크롤링 시작
-for page in range(last_page):
-
-    # 해당 페이지 안에 있는 질병 목록 확인
-    diseases = driver.find_elements(By.CLASS_NAME, 'contTitle')
-
-    # 각 질병 설명을 보기 위해 순회하면서 클릭
-    for disease in diseases:
-        disease.find_element(By.TAG_NAME, 'a').click()
-
-        # 정보 추출 (구현 예정)
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        sleep(2)
-
-        driver.back()
-
-    # 페이지 이동
-    if page != last_page-1:
-        driver.find_element(By.CLASS_NAME, 'nextPageBtn').click()
