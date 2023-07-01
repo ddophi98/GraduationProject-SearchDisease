88b3929f7e655faacf521697049946060399ed88
Yeongwoo Kim <yw1221@g.skku.edu>
2023-07-01 18:52:07 +0900

    앱 클라이언트 프로젝트 생성

---
+++ Client/Client/ClientApp.swift
@@ -0,0 +1,37 @@
+//
+//  ClientApp.swift
+//  Client
+//
+//  Created by YeongWooKim on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TabView {
+                HomeView()
+                    .tabItem {
+                        Image(systemName: "house")
+                        Text("홈 화면")
+                    }
+                
+                FindDiseaseView()
+                    .tabItem {
+                        Image(systemName: "magnifyingglass")
+                        Text("질병 검색")
+                    }
+            }
+            .tint(.black)
+        }
+    }
+}
+
+struct ApplicationContext {
+    private init() { }
+    
+    static let symptomRepository: SymptomRepository = SQLite3SymptomRepository.shared
+    static let diseaseRepository: DiseaseRepository = SQLite3DiseaseRepository.shared
+}
